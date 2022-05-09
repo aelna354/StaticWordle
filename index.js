8f6bf6704d702bb9fb222fb4f5eb2e88af3a7183
elnasrianconcept <elnasrianconcept@gmail.com>
2022-05-09 13:29:55 -0600

    Initial work

---
+++ index.js
@@ -0,0 +1,21 @@
+NUMGUESSES = 6; //Row count
+NUMLETTERS = 5; //Letter count
+
+//The current letter the user is guessing.
+var guessRow = 0;
+var guessCol = 0;
+var word = "";
+
+var gameOver = false;
+
+var correctWord = "SQUID"
+
+window.onload = function()
+{
+    initialize();
+}
+
+function initialize()
+{
+    
+}
